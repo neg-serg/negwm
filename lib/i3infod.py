9c3ce92912c995a7f13f38c2a5e584a3b8c1dff9
Neg <serg.zorg@gmail.com>
2018-03-01 01:45:40 +0300

    [i3infod] better listner

--- lib/i3infod.py
+++ lib/i3infod.py
@@ -1,14 +1,50 @@
 #!/usr/bin/env python3
 import socket
 import i3ipc
+import os
+import select
+import selectors
 import nsd
-
 from singleton import Singleton
-from threading import Event
 
 
 class BreakoutException(Exception):
     pass
+
+
+class WaitableEvent:
+    # Provides an abstract object that can be used to resume select loops with
+    # indefinite waits from another thread or process. This mimics the standard
+    # threading.Event interface.
+
+    # taken from [https://lat.sk/2015/02/multiple-event-waiting-python-3/]
+
+    def __init__(self):
+        self._read_fd, self._write_fd = os.pipe()
+
+    def wait(self, timeout=None):
+        rfds, wfds, efds = select.select([self._read_fd], [], [], timeout)
+        return self._read_fd in rfds
+
+    def isSet(self):
+        return self.wait(0)
+
+    def clear(self):
+        if self.isSet():
+            os.read(self._read_fd, 1)
+
+    def set(self):
+        if not self.isSet():
+            os.write(self._write_fd, b'1')
+
+    def fileno(self):
+        # Return the FD number of the read side of the pipe, allows this object
+        # to be used with select.select().
+        return self._read_fd
+
+    def __del__(self):
+        os.close(self._read_fd)
+        os.close(self._write_fd)
 
 
 class i3info():
@@ -20,20 +56,23 @@ class i3info():
         self.addr = '0.0.0.0'
         self.port = 31888
         self.name = ""
-        self.ws_event = Event()
-        self.ws_event.clear()
-        self.req_event = Event()
-        self.req_event.clear()
+
+        self.sel = selectors.DefaultSelector()
+        self.ws_event = WaitableEvent()
+        self.req_event = WaitableEvent()
+
+        self.sel.register(self.ws_event, selectors.EVENT_READ, "ws event")
+        self.sel.register(self.req_event, selectors.EVENT_READ, "req event")
 
         self.ns_instance = nsd.ns()
 
     def switch(self, args):
         {
-            "ns_list": self.ns_list,
+            "request": self.request,
             "reload": self.reload_config,
         }[args[0]](*args[1:])
 
-    def ns_list(self):
+    def request(self):
         self.req_event.set()
 
     def reload_config(self):
@@ -43,12 +82,9 @@ class i3info():
         self.name = event.current.name
         self.ws_event.set()
 
-    def idle_wait(self, current_conn):
-        while True:
-            if self.ws_event.wait(timeout=0.01):
-                break
-            if self.req_event.wait(timeout=0.01):
-                break
+    def idle_wait(self):
+        for ev in self.sel.select(0.01):
+            pass
         return True
 
     def listen(self):
@@ -68,18 +104,16 @@ class i3info():
                         break
                     elif 'idle' in data.decode():
                         while True:
-                            if self.idle_wait(current_conn):
+                            if self.idle_wait():
                                 current_conn.shutdown(1)
                                 current_conn.close()
-                                self.ws_event.clear()
-                                self.req_event.clear()
                                 raise BreakoutException
                     elif 'ws' in data.decode():
                         current_conn.send(self.name.encode())
                         current_conn.shutdown(1)
                         current_conn.close()
                         break
-                    elif 'ns list' in data.decode():
+                    elif 'ns_list' in data.decode():
                         output = []
                         for k in self.ns_instance.cfg:
                             output.append(k)
