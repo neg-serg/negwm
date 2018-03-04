b85d1834de6ffba3c2a981a3fdff3006caccae78
Neg <serg.zorg@gmail.com>
2018-03-05 00:56:04 +0300

    [wm3d] initial commit for zones

--- lib/wm3d.py
+++ lib/wm3d.py
@@ -11,10 +11,10 @@ class wm3(Singleton):
         self.geom_list = collections.deque([None] * 10, maxlen=10)
         self.current_resolution = self.get_screen_resolution()
         self.useless_gaps = {
-            "w": 4,
-            "a": 4,
-            "s": 4,
-            "d": 4,
+            "w": 40,
+            "a": 40,
+            "s": 40,
+            "d": 40,
         }
 
     def switch(self, args):
@@ -23,47 +23,97 @@ class wm3(Singleton):
             "focus_next_visible": self.focus_next_visible,
             "focus_prev_visible": self.focus_prev_visible,
             "maximize": self.maximize,
+            "maxhor": lambda: self.maximize(by='X'),
+            "maxvert": lambda: self.maximize(by='Y'),
+            "quad": self.quad,
             "revert_maximize": self.revert_maximize,
         }[args[0]](*args[1:])
 
     def reload_config(self):
         self.__init__()
 
-    def maximize(self, by='XY'):
-        def set_geom():
-            self.geom_list.append(
-                {
-                    "id": self.current_win.id,
-                    "geom": self.save_geom()
-                }
-            )
-            geom = self.geom_list[-1]["geom"]
+    def prev_geom(self):
+        self.geom_list.append(
+            {
+                "id": self.current_win.id,
+                "geom": self.save_geom()
+            }
+        )
+        return self.geom_list[-1]["geom"]
 
+    def quad(self, mode, use_gaps=True):
+        try:
+            mode = int(mode)
+        except TypeError:
+            print("cannot convert mode={mode} to int")
+            return
+
+        curr_scr = self.current_resolution
+        focused = self.i3.get_tree().find_focused()
+
+        if use_gaps:
+            gaps = self.useless_gaps
+        else:
+            gaps = {"w": 0, "a": 0, "s": 0, "d": 0}
+
+        if mode == 1:
+            self.set_geom(focused, {
+                'x': gaps['a'],
+                'y': gaps['w'],
+                'width': curr_scr['width'] / 2 - gaps['d'],
+                'height': curr_scr['height'] / 2 - gaps['s'],
+            })
+        elif mode == 2:
+            self.set_geom(focused, {
+                'x': curr_scr['width'] / 2 + gaps['a'],
+                'y': gaps['w'],
+                'width': curr_scr['width'] / 2 - gaps['d'] * 2,
+                'height': curr_scr['height'] / 2 - gaps['s'] * 2,
+            })
+        elif mode == 3:
+            self.set_geom(focused, {
+                'x': gaps['a'],
+                'y': gaps['w'] + curr_scr['height'] / 2,
+                'width': curr_scr['width'] / 2 - gaps['d'],
+                'height': curr_scr['height'] / 2 - gaps['s'],
+            })
+        elif mode == 4:
+            self.set_geom(focused, {
+                'x': gaps['a'] + curr_scr['width'] / 2,
+                'y': gaps['w'] + curr_scr['height'] / 2,
+                'width': curr_scr['width'] / 2 - gaps['d'],
+                'height': curr_scr['height'] / 2 - gaps['s'],
+            })
+        else:
+            return
+
+    def maximize(self, by='XY'):
         geom = {}
+
         self.current_win = self.i3.get_tree().find_focused()
         if self.current_win is not None:
             if not self.geom_list[-1]:
-                set_geom()
+                geom = self.prev_geom()
             elif self.geom_list[-1]:
-                prev = self.geom_list[-1].get("id", {})
+                prev = self.geom_list[-1].get('id', {})
                 if prev != self.current_win.id:
-                    set_geom()
+                    geom = self.prev_geom()
                 else:
                     # do nothing
                     return
-            if by == 'XY':
+            if 'XY' == by or 'YX' == by:
                 max_geom = self.maximized_geom(
                     geom.copy(),
                     byX=True,
                     byY=True
                 )
-            elif by == 'X':
+            elif 'X' == by:
                 max_geom = self.maximized_geom(
                     geom.copy(),
                     byX=True,
                     byY=False
                 )
-            elif by == 'Y':
+            elif 'Y' == by:
                 max_geom = self.maximized_geom(
                     geom.copy(),
                     byX=False,
@@ -76,18 +126,18 @@ class wm3(Singleton):
             focused = self.i3.get_tree().find_focused()
             self.set_geom(focused, self.geom_list[-1]["geom"])
             del self.geom_list[-1]
-        except:
+        except KeyError:
             pass
 
     def maximized_geom(self, geom, gaps={}, byX=False, byY=False):
         if gaps == {}:
             gaps = self.useless_gaps
         if byX:
-            geom["x"] = 0 + gaps["a"]
-            geom["width"] = self.current_resolution["width"] - gaps["d"]
+            geom['x'] = 0 + gaps['a']
+            geom['width'] = self.current_resolution['width'] - gaps['d'] * 2
         if byY:
-            geom["y"] = 0 + gaps["w"]
-            geom["height"] = self.current_resolution["height"] - gaps["s"]
+            geom['y'] = 0 + gaps['w']
+            geom['height'] = self.current_resolution['height'] - gaps['s'] * 2
         return geom
 
     def set_geom(self, win, geom):
@@ -96,10 +146,10 @@ class wm3(Singleton):
 
     def create_geom_from_rect(self, rect):
         geom = {}
-        geom["x"] = rect.x
-        geom["y"] = rect.y
-        geom["height"] = rect.height
-        geom["width"] = rect.width
+        geom['x'] = rect.x
+        geom['y'] = rect.y
+        geom['height'] = rect.height
+        geom['width'] = rect.width
 
         return geom
 
