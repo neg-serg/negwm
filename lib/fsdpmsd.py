7c7b22b95109058113a6e5f21b10df3cd4cc4b71
Neg <serg.zorg@gmail.com>
2018-03-11 18:35:42 +0300

    [fsdpmsd] fix for nsd.py

--- lib/fsdpmsd.py
+++ lib/fsdpmsd.py
@@ -1,27 +1,32 @@
-from subprocess import call
+import i3ipc
+from threading import Thread
+from subprocess import run
 from singleton import Singleton
 
 
 class fsdpms(Singleton):
     def __init__(self, i3, loop=None):
-        self.i3 = i3
+        self.i3 = i3ipc.Connection()
         self.i3.on('window::fullscreen_mode', self.on_fullscreen_mode)
         self.i3.on('window::close', self.on_window_close)
+        Thread(target=self.i3.main, daemon=True).start()
 
     def set_dpms(self, state):
         if state:
-            call(['xset', 's', 'on'])
-            call(['xset', '+dpms'])
+            run(['xset', 's', 'on'])
+            run(['xset', '+dpms'])
         else:
-            call(['xset', 's', 'off'])
-            call(['xset', '-dpms'])
+            run(['xset', 's', 'off'])
+            run(['xset', '-dpms'])
 
     def on_fullscreen_mode(self, i3, event):
+        tree = self.i3.get_tree()
         self.set_dpms(
-            not len(self.i3.get_tree().find_fullscreen())
+            not len(tree.find_fullscreen())
         )
 
     def on_window_close(self, i3, event):
-        if not len(self.i3.get_tree().find_fullscreen()):
+        tree = self.i3.get_tree()
+        if not len(tree.find_fullscreen()):
             self.set_dpms(True)
 
