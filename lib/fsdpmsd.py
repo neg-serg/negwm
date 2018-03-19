2b578eccf06c5003b9621e31db33facb69d6279b
Neg <serg.zorg@gmail.com>
2018-03-19 21:58:25 +0300

    [fsdpmsd] add docstrigs

--- lib/fsdpmsd.py
+++ lib/fsdpmsd.py
@@ -1,3 +1,9 @@
+""" Module to set / unset dpms while fullscreen is toggled on.
+
+I am simply use xset here. There is better solution possible,
+for example wayland-friendly.
+"""
+
 import i3ipc
 from threading import Thread
 from subprocess import run
@@ -6,12 +12,17 @@ from singleton import Singleton
 
 class fsdpms(Singleton):
     def __init__(self, i3, loop=None):
+        # i3ipc connection, bypassed by negi3mods runner
         self.i3 = i3ipc.Connection()
+
         self.i3.on('window::fullscreen_mode', self.on_fullscreen_mode)
         self.i3.on('window::close', self.on_window_close)
+
         Thread(target=self.i3.main, daemon=True).start()
 
     def set_dpms(self, state):
+        """ set / unset dpms
+        """
         if state:
             run(['xset', 's', 'on'])
             run(['xset', '+dpms'])
@@ -20,13 +31,25 @@ class fsdpms(Singleton):
             run(['xset', '-dpms'])
 
     def on_fullscreen_mode(self, i3, event):
+        """ Disable fullscreen if fullscreened window is here.
+
+            Args:
+                i3: i3ipc connection.
+                event: i3ipc event. We can extract window from it using
+                event.container.
+        """
         tree = self.i3.get_tree()
-        self.set_dpms(
-            not len(tree.find_fullscreen())
-        )
+        self.set_dpms(not tree.find_fullscreen())
 
     def on_window_close(self, i3, event):
+        """ Check the current fullscreen window, if no fullscreen enable dpms.
+
+            Args:
+                i3: i3ipc connection.
+                event: i3ipc event. We can extract window from it using
+                event.container.
+        """
         tree = self.i3.get_tree()
-        if not len(tree.find_fullscreen()):
+        if not tree.find_fullscreen():
             self.set_dpms(True)
 
