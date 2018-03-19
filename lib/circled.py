b15cb86075064200955bb5cf9c0013f49b584dda
Neg <serg.zorg@gmail.com>
2018-03-19 19:14:24 +0300

    [circled] add docstrings

--- lib/circled.py
+++ lib/circled.py
@@ -1,3 +1,19 @@
+""" Circle over windows module.
+
+This is a module about better run-or-raise features like in ion3, stumpwm and
+others. As the result user can get not only the usual run the appropriate
+application if it is not started, but also create a list of application, which
+I call "tag" and then switch to the next of it, instead of just simple focus.
+
+The foundation of it is pretty complicated go_next function, which use counters
+with incrementing of the current "position" of the window in the tag list over
+the finite field. As the result you get circle over all tagged windows.
+
+Also I've hacked fullscreen behaviour for it, so you can always switch to the
+window with the correct fullscreen state, where normal i3 behaviour has a lot
+of issues here in detection of existing/visible windows, etc.
+"""
+
 import re
 import os
 from modlib import Matcher
@@ -6,9 +22,30 @@ from singleton import Singleton
 
 
 class circle(modi3cfg, Matcher):
+    """ Circle over windows class
+
+    Parents:
+        modi3cfg: configuration manager to autosave/autoload
+                  TOML-configutation with inotify
+        Matcher: class to check that window can be tagged with given tag by
+                 WM_CLASS, WM_INSTANCE, regexes, etc
+
+    Metaclass:
+        Use Singleton metaclass from singleton module.
+
+    """
     __metaclass__ = Singleton
 
     def __init__(self, i3, loop=None):
+        """ Init function
+
+        Main part is in self.initialize, which performs initialization itself.
+
+        Attributes:
+            i3: i3ipc connection
+            loop: asyncio loop. It's need to be given as parameter because of
+                  you need to bypass asyncio-loop to the thread
+        """
         super().__init__(i3)
         self.initialize(i3)
         self.i3.on('window::new', self.add_wins)
@@ -17,24 +54,51 @@ class circle(modi3cfg, Matcher):
         self.i3.on("window::fullscreen_mode", self.handle_fullscreen)
 
     def initialize(self, i3):
+        # i3ipc connection, bypassed by negi3mods runner.
         self.i3 = i3
+
+        # map of tag to the tagged windows.
         self.tagged = {}
+
+        # counters for the tag [tag]
         self.counters = {}
+
+        # list of windows which fullscreen state need to be restored.
         self.restore_fullscreen = []
+
+        # is the current action caused by user actions or not? It's needed for
+        # corrent fullscreen on/off behaviour.
         self.interactive = True
+
+        # how many attempts taken to find window with priority
         self.repeats = 0
+
+        # winlist is used to reduce calling i3.get_tree() too many times.
         self.winlist = self.i3.get_tree()
+
+        # used for subtag info caching
         self.subtag_info = {}
+
+        # Should the special fullscreen-related actions to be performed or not.
         self.need_handle_fullscreen = True
 
         for tag in self.cfg:
             self.tagged[tag] = []
             self.counters[tag] = 0
 
+        # tag all windows after start
         self.tag_windows()
+
+        # store the current window here to cache get_tree().find_focused value.
         self.current_win = self.i3.get_tree().find_focused()
 
     def run_prog(self, tag, subtag=None):
+        """ Run the appropriate application for the current tag/subtag.
+
+        Attributes:
+            tag (str): denotes target [tag]
+            subtag (str): denotes the target [subtag], optional.
+        """
         try:
             if subtag is None:
                 prog_str = re.sub(
@@ -56,6 +120,13 @@ class circle(modi3cfg, Matcher):
             pass
 
     def find_prioritized_win(self, tag):
+        """ It was used as the guard to infinite loop in the past.
+
+            TODO: need refactoring.
+
+        Attributes:
+            tag (str): denotes target [tag]
+        """
         self.counters[tag] += 1
         self.repeats += 1
         if self.repeats < 8:
@@ -64,6 +135,14 @@ class circle(modi3cfg, Matcher):
             self.repeats = 0
 
     def go_next(self, tag, subtag=None):
+        """ Circle over windows. Function "called" from the user-side.
+
+            TODO: need refactoring to reduce cyclomatic complexity.
+
+        Attributes:
+            tag (str): denotes target [tag]
+            subtag (str): denotes the target [subtag], optional.
+        """
         def twin(with_subtag=False):
             if not with_subtag:
                 return self.tagged[tag][idx]
@@ -148,6 +227,16 @@ class circle(modi3cfg, Matcher):
             self.go_next(tag)
 
     def switch(self, args):
+        """ Defines pipe-based IPC for cirled module. With appropriate
+            function bindings.
+
+            This function defines bindings to the named_scratchpad methods that
+            can be used by external users as i3-bindings, sxhkd, etc. Need the
+            [send] binary which can send commands to the appropriate FIFO.
+
+            Args:
+                args (List): argument list for the selected function.
+        """
         {
             "next": self.go_next,
             "run": self.go_next,
@@ -157,6 +246,13 @@ class circle(modi3cfg, Matcher):
         }[args[0]](*args[1:])
 
     def add_prop(self, tag, prop_str):
+        """ Add property via [prop_str] to the target [tag].
+
+        Args:
+            tag (str): denotes the target tag.
+            prop_str (str): string in i3-match format used to add/delete
+                            target window in/from scratchpad.
+        """
         if tag in self.cfg:
             self.add_props(tag, prop_str)
 
@@ -166,7 +262,14 @@ class circle(modi3cfg, Matcher):
 
         self.initialize(self.i3)
 
-    def del_prop(self, tag, prop_str, full_reload=False):
+    def del_prop(self, tag, prop_str):
+        """ Delete property via [prop_str] to the target [tag].
+
+            Args:
+                tag (str): denotes the target tag.
+                prop_str (str): string in i3-match format used to add/delete
+                                target window in/from scratchpad.
+        """
         self.del_props(tag, prop_str)
 
     def tag_window(self, tag):
