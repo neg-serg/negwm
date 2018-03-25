d81e2d06205642d6ecdfb58bf4358528575bf987
Neg <serg.zorg@gmail.com>
2018-03-25 15:19:03 +0300

    [vol_printer] docstrings

--- vol_printer.py
+++ vol_printer.py
@@ -1,4 +1,34 @@
 #!/usr/bin/pypy3 -u
+""" Volume printer script
+
+This script print current MPD volume like `tail -f` echo server, so you do not
+need to use busy waiting to extract information from it.
+
+Usage:
+    ./vol_printer
+
+Suppoused to be used inside polybar.
+
+Config example:
+
+[module/volume]
+type = custom/script
+interval = 0
+exec = /home/neg/.config/i3/vol_printer.py
+exec-if = sleep 1
+tail = true
+
+Also you need to use unbuffered output for polybar, otherwise you will see no
+output at all. I've considered that pypy3 is better choise here, because of
+this application run pretty long time to get advantages of JIT compilation.
+
+Created by :: Neg
+email :: <serg.zorg@gmail.com>
+github :: https://github.com/neg-serg?tab=repositories
+year :: 2018
+
+"""
+
 import asyncio
 import sys
 
@@ -7,28 +37,46 @@ class volume_watcher():
     def __init__(self):
         self.loop = asyncio.get_event_loop()
 
-        self.ws_str = "v"
+        # default MPD address
         self.addr = "127.0.0.1"
+
+        # default MPD port
         self.port = "6600"
+
+        # buffer size
         self.buf_size = 1024
+
+        # output string
         self.volume = ""
+
+        # command to wait for mixer or player events from MPD
         self.idle_mixer = "idle mixer player\n"
+
+        # command to get status from MPD
         self.status_cmd_str = "status\n"
 
     def main(self):
+        """ Mainloop starting here.
+        """
         asyncio.set_event_loop(self.loop)
         self.loop.run_until_complete(
             self.update_mpd_volume(self.loop),
         )
 
     def pretty_printing(self, string):
+        """ Create nice and shiny output for polybar.
+        """
         return f'%{{F#395573}} || %{{F-}}%{{F#cccccc}}' + \
             f'Vol: {string}%%{{F-}}%{{F#395573}} ⟭%{{F-}}'
 
     def empty_output(self):
+        """ This output will be used if no information about volume.
+        """
         sys.stdout.write("%{{F#395573}} ⟭%{{F-}}\n")
 
     async def initial_mpd_volume(self, loop, reader, writer):
+        """ Load MPD volume state when script started.
+        """
         data = await reader.read(self.buf_size)
         writer.write(self.status_cmd_str.encode(encoding='utf-8'))
         stat_data = await reader.read(self.buf_size)
@@ -43,6 +91,8 @@ class volume_watcher():
         return data.startswith(b'OK')
 
     async def update_mpd_volume(self, loop):
+        """ Update MPD volume here and print it.
+        """
         prev_volume = 'wtf'
         reader, writer = await asyncio.open_connection(
             host=self.addr, port=self.port, loop=loop
