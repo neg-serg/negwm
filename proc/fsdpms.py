c92e3eaba14ab42ae174182ab4f563061a960b94
Neg <serg.zorg@gmail.com>
2018-08-19 03:58:20 +0300

    [fsdpms] first attempt to create autohide polybar for ns

--- proc/fsdpms.py
+++ proc/fsdpms.py
@@ -9,6 +9,7 @@ import i3ipc
 from subprocess import run
 import os
 import sys
+from collections import deque
 sys.path.append(os.getenv("XDG_CONFIG_HOME") + "/i3")
 sys.path.append(os.getenv("XDG_CONFIG_HOME") + "/i3/lib")
 from singleton import Singleton
@@ -19,14 +20,30 @@ class fsdpms(Singleton):
     def __init__(self, i3, loop=None):
         # i3ipc connection, bypassed by negi3mods runner
         self.i3 = i3ipc.Connection()
+        self.polybar_need_restore = False
+        self.focused_wins = deque(maxlen=10)
 
         self.i3.on('window::fullscreen_mode', self.on_fullscreen_mode)
         self.i3.on('window::close', self.on_window_close)
+        self.i3.on('workspace::focus', self.on_focus)
 
-    def set_dpms(self, state):
+    def on_focus(self, i3, event):
+        if 'media' in event.old.name and 'media' not in event.current.name:
+            if self.polybar_need_restore:
+                self.set_polybar(True)
+                self.polybar_need_restore = False
+
+    def set_polybar(self, on):
+        if on:
+            run(['polybar-msg', 'cmd', 'show'])
+        else:
+            run(['polybar-msg', 'cmd', 'hide'])
+        pass
+
+    def set_dpms(self, on):
         """ set / unset dpms
         """
-        if state:
+        if on:
             run(['xset', 's', 'on'])
             run(['xset', '+dpms'])
         else:
@@ -41,8 +58,16 @@ class fsdpms(Singleton):
                 event: i3ipc event. We can extract window from it using
                 event.container.
         """
+        if event.container.window_class == 'polybar':
+            return
         tree = self.i3.get_tree()
-        self.set_dpms(not tree.find_fullscreen())
+        fullscreens = tree.find_fullscreen()
+        self.set_dpms(not fullscreens)
+        if fullscreens:
+            for win in fullscreens:
+                if win.window_class == 'mpv':
+                    self.set_polybar(False)
+                    self.polybar_need_restore = True
 
     def on_window_close(self, i3, event):
         """ Check the current fullscreen window, if no fullscreen enable dpms.
@@ -52,9 +77,13 @@ class fsdpms(Singleton):
                 event: i3ipc event. We can extract window from it using
                 event.container.
         """
+        if event.container.window_class == 'polybar':
+            return
         tree = self.i3.get_tree()
-        if not tree.find_fullscreen():
+        fullscreens = tree.find_fullscreen()
+        if not fullscreens:
             self.set_dpms(True)
+            self.set_polybar(True)
 
 
 if __name__ == '__main__':
