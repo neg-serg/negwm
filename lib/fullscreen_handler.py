d8279270bebabd9f4070b6eb0fa16f67e97ea0af
Neg <serg.zorg@gmail.com>
2018-12-23 15:56:51 +0300

    [fullscreen_handler] refactoring

--- lib/fullscreen_handler.py
+++ lib/fullscreen_handler.py
@@ -5,44 +5,43 @@ I am simply use xset here. There is better solution possible,
 for example wayland-friendly.
 """
 
-
 import subprocess
 from singleton import Singleton
-from basic_config import modconfig
-from typing import List
 
 
-class fullscreen_handler(Singleton, modconfig):
+class fullscreen_handler(Singleton):
     __metaclass__ = Singleton
 
-    def __init__(self, i3, loop=None):
-        # i3ipc connection, bypassed by negi3mods runner
-        self.i3 = i3
-        self.polybar_need_restore = False
+    def __init__(self, i3, cfg):
 
-        # Initialize modcfg.
-        super().__init__(loop)
-
-        cfg = self.cfg
+        self.panel_should_be_restored = False
 
         # default panel classes
-        self.panel_classes = cfg.get("panel_classes", [])
+        self.panel_classes = cfg.panel_classes
 
         # fullscreened workspaces
-        self.ws_fullscreen = cfg.get("ws_fullscreen", [])
+        self.ws_fullscreen = cfg.ws_fullscreen
 
         # for which windows we shoudn't show panel
-        self.classes_to_hide_panel = cfg.get(
-            "classes_to_hide_panel", []
+        self.classes_to_hide_panel = cfg.classes_to_hide_panel
+
+        self.set_panel_xdo = lambda action: subprocess.Popen(
+            ['xdo', action, '-N', 'Polybar']
         )
 
-        self.set_panel = self.set_panel_xdo
+        self.set_panel_polybar = lambda action: subprocess.Popen(
+            ['polybar-msg', 'cmd', action]
+        )
 
-        # self.i3.on('window::fullscreen_mode', self.on_fullscreen_mode)
-        # self.i3.on('window::close', self.on_window_close)
-        # self.i3.on('workspace::focus', self.on_focus)
+        self.panel_action = self.set_panel_xdo
 
-    def on_focus(self, i3, event):
+    def set_panel(self, action, restore):
+        self.panel_action(action)
+        if restore is not None:
+            self.panel_should_be_restored = restore
+
+    # call it on window focus
+    def focus_action(self, i3, event):
         i3_tree = i3.get_tree()
         focused_win = i3_tree.find_focused()
         fullscreens = i3_tree.find_fullscreen()
@@ -50,65 +49,17 @@ class fullscreen_handler(Singleton, modconfig):
             if w.id == focused_win.id:
                 for ws_name in self.ws_fullscreen:
                     if ws_name in event.current.name:
-                        self.set_panel('hide')
-                        self.polybar_need_restore = True
+                        self.set_panel('hide', restore=True)
                         return
 
         for ws_name in self.ws_fullscreen:
             if ws_name in event.old.name and ws_name not in event.current.name:
-                if self.polybar_need_restore:
-                    self.set_panel('show')
-                    self.polybar_need_restore = False
+                if self.panel_should_be_restored:
+                    self.set_panel('show', restore=False)
                     return
 
-    def switch(self, args: List) -> None:
-        """ Defines pipe-based IPC for nsd module. With appropriate function
-            bindings.
-
-            This function defines bindings to the named_scratchpad methods that
-            can be used by external users as i3-bindings, sxhkd, etc. Need the
-            [send] binary which can send commands to the appropriate FIFO.
-
-            Args:
-                args (List): argument list for the selected function.
-        """
-        {
-
-        }[args[0]](*args[1:])
-
-    def set_panel_xdo(self, action):
-        subprocess.Popen(['xdo', action, '-N', 'Polybar'])
-
-    def set_panel_polybar(self, action):
-        subprocess.run(['polybar-msg', 'cmd', action])
-
-    def on_fullscreen_mode(self, i3, event):
-        """ Disable fullscreen if fullscreened window is here.
-
-            Args:
-                i3: i3ipc connection.
-                event: i3ipc event. We can extract window from it using
-                event.container.
-        """
-        if event.container.window_class in self.panel_classes:
-            return
-
-        i3_tree = self.i3.get_tree()
-        fullscreens = i3_tree.find_fullscreen()
-
-        if fullscreens:
-            focused_ws = i3_tree.find_focused().workspace().name
-            for win in fullscreens:
-                for tgt_class in self.classes_to_hide_panel:
-                    if win.window_class == tgt_class:
-                        for ws in self.ws_fullscreen:
-                            if ws in focused_ws:
-                                self.polybar_need_restore = True
-                                self.set_panel('hide')
-                                break
-                        return
-
-    def on_window_close(self, i3, event):
+    # call it on window close
+    def window_close_action(self, i3, event):
         """ Check the current fullscreen window, if no fullscreen enable dpms.
 
             Args:
@@ -119,6 +70,32 @@ class fullscreen_handler(Singleton, modconfig):
         if event.container.window_class in self.panel_classes:
             return
 
-        if not self.i3.get_tree().find_fullscreen():
+        if not i3.get_tree().find_fullscreen():
             self.set_panel('show')
+
+    # call it on fullscreen mode
+    def fullscreen_toggle_action(self, i3, event):
+        """ Disable fullscreen if fullscreened window is here.
+
+            Args:
+                i3: i3ipc connection.
+                event: i3ipc event. We can extract window from it using
+                event.container.
+        """
+        if event.container.window_class in self.panel_classes:
+            return
+
+        i3_tree = i3.get_tree()
+        fullscreens = i3_tree.find_fullscreen()
+
+        if fullscreens:
+            focused_ws = i3_tree.find_focused().workspace().name
+            for win in fullscreens:
+                for tgt_class in self.classes_to_hide_panel:
+                    if win.window_class == tgt_class:
+                        for ws in self.ws_fullscreen:
+                            if ws in focused_ws:
+                                self.set_panel('hide', restore=True)
+                                break
+                        return
 
