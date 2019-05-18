1efea1d8014dd1046b3c643201a4dacd136d8b3e
Neg <serg.zorg@gmail.com>
2019-05-18 07:51:16 +0300

    [fs] style refactoring

--- lib/fs.py
+++ lib/fs.py
@@ -11,13 +11,13 @@ from cfg import cfg
 
 
 class fs(negi3mod, cfg):
-    def __init__(self, i3, loop=None):
+    def __init__(self, i3conn, loop=None):
         # i3ipc connection, bypassed by negi3mods runner
-        self.i3 = i3
+        self.i3 = i3conn
         self.panel_should_be_restored = False
 
         # Initialize modcfg.
-        cfg.__init__(self, i3, convert_me=False)
+        cfg.__init__(self, i3conn, convert_me=False)
 
         # default panel classes
         self.panel_classes = self.cfg.get("panel_classes", [])
@@ -34,61 +34,68 @@ class fs(negi3mod, cfg):
 
         self.bindings = {
             "reload": self.reload_config,
-            "fullscreen": self.fullscreen_hide,
+            "fullscreen": self.hide,
         }
 
         self.i3.on('window::close', self.on_window_close)
         self.i3.on('workspace::focus', self.on_workspace_focus)
 
-    def on_workspace_focus(self, i3, event):
+    def on_workspace_focus(self, _, event):
+        """ Hide panel if it is fullscreen workspace, show panel otherwise """
         for tgt_ws in self.ws_fullscreen:
             if event.current.name.endswith(tgt_ws):
                 self.panel_action('hide', restore=False)
                 return
 
-        for tgt_ws in self.ws_fullscreen:
-            if not event.current.name.endswith(tgt_ws):
-                self.panel_action('show', restore=False)
-                return
+        self.panel_action('show', restore=False)
 
-    def panel_action(self, action, restore):
-        proc = subprocess.Popen(
+    def panel_action(self, action: str, restore: bool):
+        """ Helper to do show/hide with panel or another action
+
+            Args:
+                action (str): action to do.
+                restore(bool): shows should the panel state be restored or not.
+        """
+        # should be empty
+        ret = subprocess.Popen(
             ['xdo', action, '-N', 'Polybar'], stdout=subprocess.PIPE
-        )
-        proc.communicate()[0]
+        ).communicate()[0]
 
-        if restore is not None:
+        if not ret and restore is not None:
             self.panel_should_be_restored = restore
 
-    def on_fullscreen_mode(self, i3, event):
+    def on_fullscreen_mode(self, _, event):
         """ Disable panel if it was in fullscreen mode and then goes to
         windowed mode.
 
             Args:
-                i3: i3ipc connection.
+                _: i3ipc connection.
                 event: i3ipc event. We can extract window from it using
                 event.container.
         """
         if event.container.window_class in self.panel_classes:
             return
 
-        self.fullscreen_hide(i3)
+        self.hide()
 
-    def fullscreen_hide(self):
+    def hide(self):
+        """ Hide panel for this workspace """
         i3_tree = self.i3.get_tree()
         fullscreens = i3_tree.find_fullscreen()
         focused_ws = i3_tree.find_focused().workspace().name
 
-        if fullscreens:
-            for win in fullscreens:
-                for tgt_class in self.classes_to_hide_panel:
-                    if win.window_class == tgt_class:
-                        for tgt_ws in self.ws_fullscreen:
-                            if focused_ws.endswith(tgt_ws):
-                                self.panel_action('hide', restore=False)
-                                break
+        if not fullscreens:
+            return
 
-    def on_window_close(self, i3, event):
+        for win in fullscreens:
+            for tgt_class in self.classes_to_hide_panel:
+                if win.window_class == tgt_class:
+                    for tgt_ws in self.ws_fullscreen:
+                        if focused_ws.endswith(tgt_ws):
+                            self.panel_action('hide', restore=False)
+                            break
+
+    def on_window_close(self, i3conn, event):
         """ If there are no fullscreen windows then show panel closing window.
 
             Args:
@@ -100,6 +107,6 @@ class fs(negi3mod, cfg):
             return
 
         if self.show_panel_on_close:
-            if not i3.get_tree().find_fullscreen():
+            if not i3conn.get_tree().find_fullscreen():
                 self.panel_action('show', restore=True)
 
