20ad3135d1368c868b8ad1a3db61f084bb2371d9
Neg <serg.zorg@gmail.com>
2020-03-29 21:33:55 +0300

    [bin] update installer

--- bin/install.sh
+++ bin/install.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
 xdg_config_home_check() {
-    if [ -z $XDG_CONFIG_HOME ]; then
+    if [ -z "$XDG_CONFIG_HOME" ]; then
         echo "\$XDG_CONFIG_HOME should be set to install"
     fi
 
-    if [ -d $XDG_CONFIG_HOME/i3 ]; then
+    if [ -d "$XDG_CONFIG_HOME/i3" ]; then
         echo "create backup of current i3 config directory..."
-        mv -v $XDG_CONFIG_HOME/i3{,_bck}
+        mv -v "$XDG_CONFIG_HOME/i3"{,_bck}
     fi
 }
 
 pip_deps_install() {
-    echo -n [ok]
+    echo -n "[ok]"
     echo "install python dependencies..."
     sudo pip install -r ./requirements.txt --upgrade
 }
 
 git_clone() {
-    git clone git@github.com:neg-serg/negi3wm $XDG_CONFIG_HOME/i3
+    if ! which git > /dev/null; then
+        echo "Install git"
+        sudo pacman -S git --noconfirm
+    fi
+    git clone git@github.com:neg-serg/negi3wm "$XDG_CONFIG_HOME/i3"
 }
 
 change_dir() {
@@ -26,17 +30,22 @@ change_dir() {
     echo "current dir is: $(pwd)"
 }
 
-install_yay() {
-    echo "Installing yay"
+install::yay() {
+    if ! which yay > /dev/null; then
+        echo "Installing yay"
+        git clone https://aur.archlinux.org/yay.git "/tmp/yay"
+        cd "/tmp/yay"
+        makepkg -si --noconfirm
+    fi
 }
 
-python_deps_install() {
+install::python_deps() {
     echo "checking for pip3 installed..."
     if which pip3 > /dev/null; then
         pip_deps_install
     else
         echo "trying to install python-pip..."
-        if which pacman > /dev/null; then
+        if which yay > /dev/null; then
             yay -S python-pip --noconfirm
         else
             echo "Currently only Arch linux is supported with yay installer"
@@ -50,20 +59,20 @@ python_deps_install() {
     fi
 }
 
-install_mandatory_deps() {
-    install_deps i3 ppi3 dash
+install::mandatory_deps() {
+    install::deps i3 ppi3 dash
 }
 
-install_recommended_deps() {
-    install_deps zsh tmux rofi dunst xdo alacritty pulseaudio
+install::recommended_deps() {
+    install::deps zsh tmux rofi dunst xdo alacritty pulseaudio
 }
 
-install_deps() {
+install::deps() {
     for dep in "$@"; do
         echo -n "Check for $dep..."
-        if ! which $dep > /dev/null; then
+        if ! which "$dep" > /dev/null; then
             echo "Install $dep..."
-            yay -S $dep --noconfirm || yay -S $dep-git --noconfirm
+            yay -S "$dep" --noconfirm || yay -S "$dep-git" --noconfirm
         else
             echo -n " $dep is already installed [OK]"
         fi
@@ -75,9 +84,10 @@ main(){
     xdg_config_home_check
     git_clone
     change_dir
-    python_deps_install
-    install_mandatory_deps
-    install_recommended_deps
+    install::yay
+    install::python_deps
+    install::mandatory_deps
+    install::recommended_deps
 }
 
 main "$@"
