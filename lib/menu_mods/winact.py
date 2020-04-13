e4da61c77d481bcd064abd01d48f086437c441f1
Neg <serg.zorg@gmail.com>
2020-04-13 04:41:30 +0300

    [menu_mods] better ws menu

--- lib/menu_mods/winact.py
+++ lib/menu_mods/winact.py
@@ -6,7 +6,6 @@ from typing import Callable
 class winact():
     def __init__(self, menu):
         self.menu = menu
-        self.workspaces = menu.conf("workspaces")
 
     def win_act_simple(self, cmd: str, prompt: str) -> None:
         """ Run simple and fast selection dialog for window with given action.
@@ -22,6 +21,8 @@ class winact():
             'width': int(self.menu.screen_width * 0.75),
             'prompt': f"{prompt} {self.menu.conf('prompt')}"
         }
+
+        win_name = None
         if winlist and winlist_len > 1:
             win_name = subprocess.run(
                 self.menu.args(menu_params),
@@ -39,8 +40,7 @@ class winact():
                     win.command(cmd)
 
     def goto_win(self) -> None:
-        """ Run menu goto selection dialog
-        """
+        """ Run menu goto selection dialog """
         self.win_act_simple('focus', self.menu.wrap_str('go'))
 
     def attach_win(self) -> None:
@@ -50,52 +50,67 @@ class winact():
             'move window to workspace current', self.menu.wrap_str('attach')
         )
 
-    def select_ws(self, use_wslist: bool) -> str:
-        """ Apply target function to workspace.
-        """
-        if use_wslist:
-            wslist = self.workspaces
-        else:
-            wslist = [ws.name for ws in self.menu.i3ipc.get_workspaces()] + \
-                ["[empty]"]
+    def select_ws(self) -> str:
+        """ Apply target function to workspace. """
+        ws_list_internal = self.menu.i3ipc.get_workspaces()
         menu_params = {
-            'cnum': len(wslist),
+            'cnum': len(ws_list_internal),
             'width': int(self.menu.screen_width * 0.66),
             'prompt': f'{self.menu.wrap_str("ws")} {self.menu.conf("prompt")}',
         }
-        workspace_name = subprocess.run(
-            self.menu.args(menu_params),
-            stdout=subprocess.PIPE,
-            input=bytes('\n'.join(wslist), 'UTF-8'),
-            check=False
-        ).stdout
+        wslist_fancy = {'names':[], 'ids':[]}
+        try:
+            for index, ws in enumerate(ws_list_internal):
+                ws_split = ws.name.split('::')
+                wslist_fancy['names'].append(''.join(ws_split[1:]))
+                wslist_fancy['ids'].append(
+                    {'index' :index, 'num': int(ws_split[0])}
+                )
+            workspace_name = subprocess.run(
+                self.menu.args(menu_params),
+                stdout=subprocess.PIPE,
+                input=bytes('\n'.join(wslist_fancy['names']), 'UTF-8'),
+                check=False
+            ).stdout
 
-        selected_ws = workspace_name.decode('UTF-8').strip()
-        return str(wslist.index(selected_ws) + 1) + ' :: ' + selected_ws
+            selected_ws = workspace_name.decode('UTF-8').strip()
+            if selected_ws:
+                names = wslist_fancy['names']
+                for wid in wslist_fancy['ids']:
+                    if names[wid['index']].strip() == selected_ws:
+                        return str(wid['num']) + ' :: ' + selected_ws
+        except Exception:
+            return ''
+        return ''
 
     @staticmethod
     def apply_to_ws(ws_func: Callable) -> None:
-        """ Partial apply function to workspace.
-        """
+        """ Partial apply function to workspace. """
         ws_func()
 
-    def goto_ws(self, use_wslist: bool = True) -> None:
-        """ Go to workspace menu.
-        """
-        workspace_name = self.select_ws(use_wslist)
+    def goto_ws(self) -> None:
+        """ Go to workspace menu. """
+        workspace_name = self.select_ws()
         if workspace_name is not None and workspace_name:
-            self.apply_to_ws(
-                partial(self.menu.i3ipc.command, f'workspace {workspace_name}')
-            )
+            ws_splitname = workspace_name.split('::')
+            if len(ws_splitname) > 1:
+                ws_num = ws_splitname[0].strip()
+                self.apply_to_ws(
+                    partial(
+                        self.menu.i3ipc.command, f'workspace number {ws_num}'
+                    )
+                )
 
-    def move_to_ws(self, use_wslist: bool = True) -> None:
-        """ Move current window to the selected workspace
-        """
-        workspace_name = self.select_ws(use_wslist)
+    def move_to_ws(self) -> None:
+        """ Move current window to the selected workspace """
+        workspace_name = self.select_ws()
         if workspace_name is not None and workspace_name:
-            self.apply_to_ws(
-                partial(self.menu.i3ipc.command,
-                        f'[con_id=__focused__] \
-                        move to workspace {workspace_name}')
-            )
+            ws_splitname = workspace_name.split('::')
+            if len(ws_splitname) > 1:
+                ws_num = ws_splitname[0].strip()
+                self.apply_to_ws(
+                    partial(self.menu.i3ipc.command,
+                            f'[con_id=__focused__] \
+                            move to workspace number {ws_num}')
+                )
 
