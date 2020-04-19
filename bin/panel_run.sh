d577b8dbc687c2a3d8e301429d5f78ae3f084263
Neg <serg.zorg@gmail.com>
2020-04-19 22:27:02 +0300

    [bin] panel_run cleanup

--- bin/panel_run.sh
+++ bin/panel_run.sh
@@ -1,5 +1,4 @@
 #!/bin/dash
-
 export POLYBAR_INPUT_FORMAT="input:%layout%"
 export POLYBAR_MPD_FORMAT_PAUSED="%{F#005f87}paused%{F#395573} %{F#395573}" 
 export POLYBAR_MPD_LEFT_SIDE="%{F#005f87}〉%{F#005fd7}〉%{F#395573} %{F-}"
@@ -10,7 +9,6 @@ export POLYBAR_LL="%{F-}"
 export POLYBAR_RR="%{F-}"
 
 pkill -x polybar
-
 if [ $(pgrep -x polybar|wc -l) -le 1  ]; then
     polybar -c ${XDG_CONFIG_HOME}/polybar/main main &
 fi
