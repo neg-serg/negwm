068d9040ab6d1d93499aefcb2d6e8167842a00d3
Neg <serg.zorg@gmail.com>
2020-11-30 02:52:27 +0300

    [menu] CS

--- lib/menu.py
+++ lib/menu.py
@@ -1,14 +1,10 @@
-""" Menu manager module.
-
-    This module is about creating various menu.
-
-    For now it contains following menus:
-        - Goto workspace.
-        - Attach to workspace.
-        - Add window to named scratchpad or circle group.
-        - xprop menu to get X11-atom parameters of selected window.
-        - i3-cmd menu with autocompletion.
-"""
+""" Menu manager module. This module is about creating various menu. For now it
+contains following menus:
+    - Goto workspace.
+    - Attach to workspace.
+    - Add window to named scratchpad or circle group.
+    - xprop menu to get X11-atom parameters of selected window.
+    - i3-cmd menu with autocompletion. """
 
 import importlib
 from typing import List
@@ -20,69 +16,45 @@ from .extension import extension
 
 class menu(extension, cfg):
     """ Base class for menu module """
-
     def __init__(self, i3ipc) -> None:
-        # Initialize cfg.
         cfg.__init__(self, i3ipc)
-
-        # i3ipc connection, bypassed by negi3wm runner
         self.i3ipc = i3ipc
-
-        # i3 path used to get "send" binary path
         self.i3_path = Misc.i3path()
-
-        # i3-msg application name
-        self.i3cmd = self.conf("i3cmd")
-
+        self.i3cmd = self.conf("i3cmd") # i3-msg application name
         # Window properties shown by xprop menu.
         self.xprops_list = self.conf("xprops_list")
-
-        # cache screen width
+        # Cache screen width
         if not self.conf("use_default_width"):
             from display import Display
             self.screen_width = Display.get_screen_resolution()["width"]
         else:
             self.screen_width = int(self.conf('use_default_width'))
-
         for mod in self.cfg['modules']:
             module = importlib.import_module('menu_mods.' + mod)
             setattr(self, mod, getattr(module, mod)(self))
-
         self.bindings = {
             "cmd_menu": self.i3menu.cmd_menu,
-
             "xprop": self.xprop.xprop,
-
             "autoprop": self.props.autoprop,
             "show_props": self.props.show_props,
-
             "pulse_output": self.pulse_menu.pulseaudio_output,
             "pulse_input": self.pulse_menu.pulseaudio_input,
             "pulse_mute": self.pulse_menu.pulseaudio_mute,
-
             "ws": self.winact.goto_ws,
             "goto_win": self.winact.goto_win,
             "attach": self.winact.attach_win,
             "movews": self.winact.move_to_ws,
-
             "gtk_theme": self.gnome.change_gtk_theme,
             "icon_theme": self.gnome.change_icon_theme,
-
             "xrandr_resolution": self.xrandr.change_resolution_xrandr,
-
             "reload": self.reload_config,
         }
 
     def args(self, params: dict) -> List[str]:
         """ Create run parameters to spawn rofi process from dict
-
-            Args:
-                params(dict): parameters for rofi
-            Return:
-                List(str) to do rofi subprocessing
-        """
+            params(dict): parameters for rofi
+            List(str) to do rofi subprocessing """
         prompt = self.conf("prompt")
-
         params['width'] = params.get('width', int(self.screen_width * 0.85))
         params['prompt'] = params.get('prompt', prompt)
         params['cnum'] = params.get('cnum', 16)
@@ -90,7 +62,6 @@ class menu(extension, cfg):
         params['markup_rows'] = params.get('markup_rows', '-no-markup-rows')
         params['auto_selection'] = \
             params.get('auto_selection', "-no-auto-selection")
-
         matching = self.conf("matching")
         return [
             'rofi', '-show', '-dmenu',
