379114c8ae1b73bb5faaa9a2f9f2c54629c20507
Neg <serg.zorg@gmail.com>
2020-11-30 02:52:48 +0300

    [locker] CS

--- lib/locker.py
+++ lib/locker.py
@@ -1,22 +1,16 @@
 """ Create a pid lock with abstract socket.
-
     Taken from [https://stackoverflow.com/questions/788411/check-to-see-if-python-script-is-running]
 """
 
 import sys
 import socket
 
-
 def get_lock(process_name: str, verbose=False) -> None:
     """
     Without holding a reference to our socket somewhere it gets garbage
     collected when the function exits
-
-    Args:
-        process_name (str): process name to bind.
-    """
+    process_name (str): process name to bind. """
     get_lock._lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-
     try:
         get_lock._lock_socket.bind('\0' + process_name)
         if verbose:
