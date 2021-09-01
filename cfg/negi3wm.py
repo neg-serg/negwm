cec3ae6e75ccbbe208b496e3a02458f1c9b30a1a
Neg <serg.zorg@gmail.com>
2021-09-01 18:45:44 +0300

    [negi3wm] add config

---
+++ cfg/negi3wm.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class Negi3wm(Enum):
+    prefix = '❯>'
+    module_list = [
+        'fullscreen',
+        'executor',
+        'actions',
+        'circle',
+        'remember_focused',
+        'menu',
+        'scratchpad',
+        'conf_gen'
+    ]
+    port = '15555'
