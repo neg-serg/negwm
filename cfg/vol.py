635b634f6e3cae90e451ea2fccd7d064c6ac48ab
Neg <serg.zorg@gmail.com>
2021-09-01 18:36:53 +0300

    [vol] remove

--- cfg/vol.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Vol(Enum):
-    mpd_addr = '::1'
-    mpd_buf_size = 1024
-    mpd_inc = 1
-    mpd_port = '6600'
-    mpv_socket = '/tmp/mpv.socket'
-    use_mpv09 = 1
