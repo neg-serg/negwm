48114dc0b0af2ce1fcaba46df03372df1f287e46
Neg <serg.zorg@gmail.com>
2021-11-11 17:58:09 +0300

    [scratchpad] new ncmpcpp geom

--- cfg/scratchpad.py
+++ cfg/scratchpad.py
@@ -22,7 +22,7 @@ class scratchpad(Enum):
     )
 
     ncmpcpp = Δ(
-        geom = '2251x844+793+971',
+        geom = '2251x852+753+1181',
         instance = ['ncmpcpp'],
         keybind_default_toggle = ['Mod4+f'],
         spawn = 'ncmpcpp',
