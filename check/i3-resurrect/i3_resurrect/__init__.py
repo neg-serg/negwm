784894b534e4caf445da33d85693015688ac25c6
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-09-28 00:25:27 +0300

    [check] cleanup

--- check/i3-resurrect/i3_resurrect/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = ['config', 'layout', 'main', 'programs', 'treeutils', 'util']
-
-from . import config
-from . import layout
-from . import main
-from . import programs
-from . import treeutils
-from . import util
