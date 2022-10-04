07b52e18b509ee9522d564fe5673ee6b9044484c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-10-04 19:50:53 +0300

    [scratchpad] CS

--- cfg/scratchpad.py
+++ cfg/scratchpad.py
@@ -1,16 +1,14 @@
 from enum import Enum
 Δ = dict
 
-def discord():
-    return Δ(
+def discord(): return Δ(
         classw = ['discord'],
         geom = '1898x1273+1936+825',
         keybind_toggle = ['Mod4+v'],
         prog = 'discord'
     )
 
-def im():
-    return Δ(
+def im(): return Δ(
         classw = ['KotatogramDesktop', 'vkteams', 'Skype', 'Slack', 'TelegramDesktop', 'zoom'],
         geom = '1304x2109+2536+2',
         keybind_toggle = ['Mod4+e'],
@@ -21,16 +19,14 @@ def im():
         )
     )
 
-def ncmpcpp():
-    return Δ(
+def ncmpcpp(): return Δ(
         geom = '2251x828+753+1205',
         classw = ['ncmpcpp'],
         keybind_toggle = ['Mod4+f'],
         spawn = 'ncmpcpp',
     )
 
-def neomutt():
-    return Δ(
+def neomutt(): return Δ(
         classw = ['mutterfox'],
         geom = '3670x2228+104+0',
         instance = ['mutterfox', 'neomutt'],
@@ -38,24 +34,21 @@ def neomutt():
         spawn = 'neomutt',
     )
 
-def teardrop():
-    return Δ(
+def teardrop(): return Δ(
         geom = '3840x1300+0+0',
         instance = ['teardrop'],
         keybind_toggle = ['Mod4+d'],
         spawn = 'teardrop'
     )
 
-def torrment():
-    return Δ(
+def torrment(): return Δ(
         geom = '3840x1300+0+0',
         instance = ['torrment'],
         keybind_toggle = ['Mod4+t'],
         spawn = 'torrment'
     )
 
-def transients():
-    return Δ(
+def transients(): return Δ(
         geom = '1812x797+693+1310',
         match_all = ['True'],
         keybind_spec_toggle = ['a'],
@@ -63,8 +56,7 @@ def transients():
     )
 
 
-def webcam():
-    return Δ(
+def webcam(): return Δ(
         geom = '2463x1880+1368+268',
         instance = ['webcam'],
         keybind_spec_toggle = ['w'],
