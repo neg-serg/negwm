08e41b5e412df238f78d5a96e3f3bcf388a818d5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-10-18 15:49:30 +0300

    [negwm] bump version

--- negwm.py
+++ negwm.py
@@ -205,7 +205,7 @@ def main():
     get_lock(os.path.basename(__file__))
     # We need it because of thread_wait on Ctrl-C.
     atexit.register(cleanup)
-    docopt(str(__doc__), version='0.9')
+    docopt(str(__doc__), version='0.9.1')
     wm = negwm()
     wm.run()
 
