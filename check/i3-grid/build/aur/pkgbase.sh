23003ed7d1a5e2b94df25b655f8093f369f70068
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-12-19 19:41:14 +0300

    [check] cleanup

--- check/i3-grid/build/aur/pkgbase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone ssh://aur@aur.archlinux.org/python-i3-grid
-cp ./PKGBUILD ./python-i3-grid
-cd python-i3-grid && makepkg --printsrcinfo > .SRCINFO
-
-echo "Updated i3-grid AUR Repo. Add the files and push commit to AUR:"
-echo -e "\tgit add PKGBUILD .SRCINFO\n\tgit commit -m <commitName>\n\tgit push "
