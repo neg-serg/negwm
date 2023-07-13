51cc1a75dbc009559dd4b528edd525b3d73dae88
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-07-14 00:26:49 +0300

    [executor] refactoring

---
+++ negwm/lib/execenv.py
@@ -0,0 +1,227 @@
+import shutil
+import threading
+import multiprocessing
+import logging
+from os.path import expanduser
+from negwm.lib.misc import Misc
+
+class execenv():
+    ''' Environment class. It is a helper for tmux manager to store info about currently selected application. This class rules over
+    parameters and settings of application, like used terminal enumator, fonts, all path settings, etc.
+    config: manager to autosave/autoload configutation with inotify '''
+    cache = Misc.cache_path()
+    paths = {}
+
+    def __init__(self, name: str, config) -> None:
+        self.name, self.config = name, config
+        if not self.cfg_block():
+            logging.error(f'Cannot create config for {name}, with config {config}')
+            return
+        Misc.create_dir(Misc.cache_path())
+        blk = self.cfg_block()
+        term = self.term()
+        blk.setdefault('exec', [])
+        blk.setdefault('exec_tmux', [])
+        blk.setdefault('exec_dtach', [])
+        self.exec_tmux = blk['exec_tmux']
+        if not self.exec_tmux:
+            exec_dtach = blk['exec_dtach']
+            if not exec_dtach:
+                self.exec = blk['exec']
+            else:
+                env.prepare_dtach()
+                self.exec = f'dtach -A {env.paths["dtach_session_dir"]}' \
+                            f'/{name}.session {exec_dtach}'
+        blk.setdefault('classw', term)
+        self.wclass = blk['classw']
+        self.opts = getattr(self, term)()
+
+        def join_processes():
+            for prc in multiprocessing.active_children():
+                prc.join()
+        threading.Thread(target=join_processes, args=(), daemon=True).start()
+
+    @Misc.run_once
+    @staticmethod
+    def prepare_alacritty():
+        env.paths['alacritty_cfg_dir'] = f'{env.cache}/alacritty_cfg'
+        env.paths['alacritty_cfg'] = expanduser(f'{Misc.xdg_config_home()}/alacritty/alacritty.yml')
+        Misc.create_dir(env.paths['alacritty_cfg_dir'])
+
+    @Misc.run_once
+    @staticmethod
+    def prepare_tmux():
+        env.paths['tmux_socket_dir'] = f'{env.cache}/tmux_sockets'
+        Misc.create_dir(env.paths['tmux_socket_dir'])
+
+    @Misc.run_once
+    @staticmethod
+    def prepare_dtach():
+        env.paths['dtach_session_dir'] = f'{env.cache}/dtach_sessions'
+        Misc.create_dir(env.paths['dtach_session_dir'])
+
+    @staticmethod
+    def tmux_socket_path(name):
+        return expanduser(f'{env.paths["tmux_socket_dir"]}/{name}.socket')
+
+    @staticmethod
+    def tmux_session_attach(name):
+        return f'tmux -S {env.tmux_socket_path(name)} a -t {name}'
+
+    @staticmethod
+    def tmux_new_session(name):
+        return f'tmux -S {env.tmux_socket_path(name)} new-session -s {name}'
+
+    def cfg_block(self) -> dict:
+        return self.config.get(self.name, {})
+
+    def shell(self) -> str:
+        return self.cfg_block().get('shell', 'dash')
+
+    def font(self) -> str:
+        ret = self.config.get('default_font', '')
+        if not ret:
+            ret = self.cfg_block().get('font', 'Iosevka')
+        return ret
+
+    def font_size(self) -> str:
+        return self.cfg_block().get('font_size', '14')
+
+    def term(self):
+        term = self.cfg_block().get('term', '')
+        if term:
+            return term
+        # Detect fallback terminal
+        for t in ['alacritty', 'kitty', 'st', 'zutty']:
+            if shutil.which(t):
+                return t
+        logging.error('No supported terminal installed, fail :(')
+        return ''
+
+    def style(self) -> dict:
+        use_one_fontstyle = self.config.get('use_one_fontstyle', False)
+        style = self.config.get('default_style', '')
+        if not style:
+            style = self.cfg_block().get('style', 'Regular')
+        if use_one_fontstyle:
+            return {
+                'normal': self.cfg_block().get('font_normal', style),
+                'bold' : self.cfg_block().get('font_bold', style),
+                'italic': self.cfg_block().get('font_italic', style)
+            }
+        else:
+            return {
+                'normal': self.cfg_block().get('font_normal', 'Regular'),
+                'bold' : self.cfg_block().get('font_bold', 'Bold'),
+                'italic': self.cfg_block().get('font_italic', 'Italic')
+            }
+
+    def alacritty(self) -> str:
+        env.prepare_alacritty()
+
+        def create_alacritty_cfg(name: str) -> str:
+            ''' Config generator for alacritty. We need it because of alacritty
+            cannot bypass most of user parameters with command line now.
+            cfg_dir: alacritty config dir
+            config: config dirtionary
+            name(str): name of config to generate
+            cfgname(str): configname '''
+            app_name = self.config.get(name, {}).get('app_name', '')
+            if not app_name:
+                app_name = self.config.get(name, {}).get('classw')
+            app_name = f'{app_name}.yml'
+            cfgname = expanduser(f'{env.paths["alacritty_cfg_dir"]}/{app_name}')
+            shutil.copyfile(env.paths["alacritty_cfg"], cfgname)
+            return cfgname
+
+        def alacritty_yml_create(custom_config: str) -> None:
+            ''' Create config for alacritty
+            custom_config(str): config name to create '''
+            import yaml
+            import yamlloader
+            conf = None
+            with open(custom_config, 'r', encoding='utf-8') as cfg_file:
+                try:
+                    conf = yaml.load(
+                        cfg_file, Loader=yamlloader.ordereddict.CSafeLoader)
+                    if conf is not None:
+                        if 'font' in conf:
+                            font = conf['font']
+                            for w in 'normal', 'bold', 'italic':
+                                font[w]['family'] = self.font()
+                                font[w]['style'] = self.style()[w]
+                            font['size'] = self.font_size()
+                        if 'window' in conf:
+                            window = conf['window']
+                            padding = self.cfg_block().get('padding', [2, 2])
+                            opacity = self.cfg_block().get('opacity', 0.88)
+                            window['opacity'] = opacity
+                            window['padding']['x'] = int(padding[0])
+                            window['padding']['y'] = int(padding[1])
+                except yaml.YAMLError as yamlerror:
+                    logging.error(yamlerror)
+
+            if conf is not None and conf:
+                with open(custom_config, 'w', encoding='utf8') as outfile:
+                    try:
+                        yaml.dump(
+                            conf,
+                            outfile,
+                            default_flow_style=False,
+                            allow_unicode=True,
+                            canonical=False,
+                            explicit_start=True,
+                            Dumper=yamlloader.ordereddict.CDumper
+                        )
+                    except yaml.YAMLError as yamlerror:
+                        logging.error(yamlerror)
+
+        self.title = self.cfg_block().get('title', self.wclass)
+        custom_config = create_alacritty_cfg(self.name)
+        multiprocessing.Process(
+            target=alacritty_yml_create, args=(custom_config,),
+            daemon=True
+        ).start()
+
+        return ' '.join([
+            f'{self.term()}',
+            f'--config-file {expanduser(custom_config)}',
+            f'--class {self.wclass},{self.wclass}',
+            f'-t {self.title} -e'])
+
+    def st(self):
+        return ' '.join([
+            f'{self.term()}',
+            f'-c {self.wclass}',
+            f'-t {self.name}',
+            f"-f {self.font} :size={str(self.font_size())}:style={self.style()['normal']}",
+            '-e'])
+
+    def kitty(self):
+        cfg = self.cfg_block()
+        padding = cfg.get('padding', [0, 0])[0]
+        opacity = cfg.get('opacity', 0.88)
+        instance_group = cfg.get('instance_group', self.name)
+        style = self.style()['normal']
+        if style == 'Regular':
+            style=''
+        else:
+            style=f' {style}'
+        font_settings=[
+            f'-o font_family=\'{self.font()}{style}\'',
+            f'-o font_size={str(self.font_size())}'
+        ]
+        return ' '.join([
+            f'{self.term()}',
+            '-1', f'--instance-group {instance_group}',
+            f'--class={self.wclass}',
+            f'--title={self.name}',
+            f'-o window_padding_width={padding}',
+            f'-o background_opacity={opacity}'] + font_settings)
+
+    def zutty(self):
+        return ' '.join([
+            f'{self.term()}',
+            f'-name {self.wclass}',
+            f'-font {self.font}',
+            f'-fontsize {str(self.font_size())}'])
