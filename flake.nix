186c80b4cb984b2f294db6502ec3fe4b34eda14c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-25 13:56:50 +0300

    [nix] try port

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Application packaged using poetry2nix";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+        propagatedBuildInputs = [
+            pkgs.python3.xlib
+        ];
+      in
+      {
+        packages = {
+          negwm = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.negwm;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.negwm ];
+          packages = [ pkgs.poetry ];
+        };
+      });
+}
